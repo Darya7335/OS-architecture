b1d8ba374e7857c3a20d6bf1afb0c71d91a5c127
Darya7335 <dashaivanova.1998@mail.ru>
2022-03-27 13:13:54 -0700

    add test

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-sudo ./build/rest_server --host=localhost --port=3306 --login=root --password=passworD1. --database=work
+sudo ./build/rest_api --host=localhost --port=3306 --login=root --password=passworD1. --database=work
 
---
+++ test.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+#include "database/database.h"
+#include "database/person.h"
+#include "config/config.h"
+
+TEST(first_check, basic_test_set)   //тест теста
+{
+    std::string output = "Hello world";
+    ASSERT_TRUE(output == "Hello world");
+}
+
+TEST(check_login, basic_test_set) {  //проверка get_login()
+    database::Person person;
+    person.login() = "Bradley58";
+    EXPECT_EQ("Bradley58", person.get_login());
+}
+
+TEST(read_by_login, basic_test_set) {
+    try
+    {
+        database::Person result = database::Person::read_by_login("Bradley58");
+        EXPECT_EQ("Bradley58", result.get_login());
+    }
+    catch (...)
+    {
+        ASSERT_TRUE(false);
+    }
+} 
+
+TEST(check_create_person, basic_test_set) {
+    database::Person person;
+    person.login()      = "Ivan25";
+    person.first_name() = "Ivan";
+    person.last_name()  = "Ivanov";
+    person.age()        = 25;
+
+    try
+    {
+        person.save_to_mysql();
+        ASSERT_TRUE(true);
+    }
+    catch (...)
+    {
+        ASSERT_TRUE(false);
+    }
+}
+
+int main(int argc, char *argv[]){
+    testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
